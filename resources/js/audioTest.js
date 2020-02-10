fda1c50e447af52ca81022d9463d1fd6eebb1d40
schans2 <schantz.samuel@gmail.com>
2020-02-09 20:14:15 -0500

    Queue system sort of works

--- resources/js/audioTest.js
+++ resources/js/audioTest.js
@@ -17,8 +17,38 @@ function musicListeners() {
   });
   $("#choice>li").click(function() {
     $("#queue").append($(this).clone());
+    if($("#queue>li").length === 1) {
+      decisionizer();
+    }
     $("#queue>li").click(function() {
       $(this).remove();
     });
   });
+  // $("audio").bind('ended', function() {
+  //   alert("Song ended");
+  // });
+  $("#qPlay").bind('ended', function() {
+    $("#queue>li:nth-child(1)").remove();
+    decisionizer();
+  });
+}
+
+function decisionizer() {
+  if($("#queue>li").length) {
+    if($("#queue>li:nth-child(1)").attr("id") === "audio") {
+      $("#qPlay").attr("src", "resources/music/08 I Am the Highway.m4a");
+    }
+    else if($("#queue>li:nth-child(1)").attr("id") === "metal") {
+      $("#qPlay").attr("src", "resources/music/01 Enter Sandman.m4a");
+    }
+    else if($("#queue>li:nth-child(1)").attr("id") === "weez") {
+      $("#qPlay").attr("src", "resources/music/03 Hash Pipe.m4a");
+    }
+    $("#qPlay")[0].play();
+  }
+  else {
+    $("#qPlay")[0].pause();
+    $("#qPlay").attr("src", "#");
+    alert("End of queue");
+  }
 }
