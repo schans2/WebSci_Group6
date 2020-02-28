de1dc0b6e132a7f8c7109cde0cb834d124082c0d
xinyans <sunx15@rpi.edu>
2020-02-28 10:07:10 -0500

    Some Changes

--- server/server.js
+++ server/server.js
@@ -11,8 +11,13 @@ var client_id = 'd26cbce7996941a29fc11401be4ecb98';  // Your client id
 var client_secret = 'a66d2b0d602b4d439fbae508e0597ff5';  // Your secret, be sure to delete this part when making public!
 var redirect_uri = 'http://localhost:8888/callback';  // Your redirect uri
 
+app.get('/login', (req, res) => {
+    res.send("https://www.youtube.com");
+});
 
-
+app.use(function(req, res){
+    console.log(req);
+});
 
 
 
@@ -22,5 +27,5 @@ var redirect_uri = 'http://localhost:8888/callback';  // Your redirect uri
 
 
 
-console.log('Listening on 8888');
-app.listen(8888);
+console.log('Listening on 8889');
+app.listen(8889);
