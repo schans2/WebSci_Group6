49b9441a3db8b42db18ca992c53b1af771b4fc81
xinyans <sunx15@rpi.edu>
2020-03-01 14:50:45 -0500

    Voting System Prototype

--- resources/js/vote.js
+++ resources/js/vote.js
@@ -1,5 +1,27 @@
 var voteAngularApp = angular.module('voteApp', []);
 
 voteAngularApp.controller('voteController', ['$scope', function($scope){
-    
+    // Music data from a collaborative playlist, later to be dynamically fetched from the database
+    $scope.music_data = [
+        {
+            "name": "Song A",
+            "upvotes": 100,
+            "downvotes": 20
+        },
+        {
+            "name": "Song B",
+            "upvotes": 10,
+            "downvotes": 10
+        },
+        {
+            "name": "Song C",
+            "upvotes": 5,
+            "downvotes": 100
+        },
+        {
+            "name": "Song D",
+            "upvotes": 200,
+            "downvotes": 180
+        }
+    ];
 }]);
