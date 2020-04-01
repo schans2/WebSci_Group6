b6c9539620f6161a9321351341bde7d5409747e5
xinyans <sunx15@rpi.edu>
2020-04-01 00:31:26 -0400

    Static Changes

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var app = express();
 var port = 3000;
 
 // Public static files to serve
-app.use(express.static('resources'));
+app.use('/resources', express.static('resources'));
 
-// Routing
+// Routing to serve pages
 app.get('/player', function(req, res){
     res.sendFile(__dirname + "/music_player.html");
 });
@@ -24,6 +24,27 @@ app.get('/friends', function(req, res){
 app.get('/login', function(req, res){
     res.sendFile(__dirname + "/login.html");
 });
+
+app.get('/user', function(req, res){
+    res.sendFile(__dirname + "/userPage.html");
+});
+
+// API calls to the server
+app.post('/join', function(req, res){
+    // verify user login status, join user to a group, and redirect.
+    // TO BE IMPLEMENTED
+    return
+});
+
+app.post('/login', function(req, res){
+    // verify user credentials, and log user in.
+    // TO BE IMPLEMENTED
+});
+
+app.post('/logout', function(req, res){
+    // clear user login status
+    // TO BE IMPLEMENTED
+})
 
 
 
