fc03f95954fde33cc31234f7e3c56943c43f9264
xinyans <sunx15@rpi.edu>
2020-04-17 10:37:36 -0400

    Angular login stuff

--- resources/js/login.js
+++ resources/js/login.js
@@ -22,15 +22,19 @@ angular.module('loginApp', [])
           });
         }
         $scope.loginUser = function(){
-          var uname = document.getElementById("username").value;
-          var pass = document.getElementById("password").value;
-
           $scope.user = {
-            uname: uname,
-            pass: pass
+            uname: $scope.uname,
+            pass: $scope.pass
           }
           $http.post('/login', $scope.user).then(function(response){
-            console.log("post successful");
+            console.log(response.data);
+            if(!response.data.error){
+              alert("Login Successful!")
+              window.location.href = "/user";
+            }
+            else{
+              alert("Username or password incorrect!");
+            }
           });
         }
     }]);
@@ -79,40 +83,41 @@ function eventListeners() {
       alert("Error: Please make sure all fields are filled out.");
     }
   });
-  $("#signIn").click(function() {
-    uname = $("#username").val();
-    pass = $("#password").val();
-    // Send Credentials to Server
-    if(uname && pass) {
-      $.ajax({
-        method    : "POST",
-        url       : "/login",
-        data      : {
-          "uname" : uname,
-          "pass"  : pass
-        },
-        success: function(message, status){
-          console.log(`Register request returns success with message ${message} and status ${status}.`);
-          alert("Login successful!");
-          window.location.href = "/user";
-        },
-        error: function(message, status){
-          console.log(`Oops! Register request returns failure with message ${message} and status ${status}.`);
-        }
-      });
-    }
-    else {
-      alert("Error: Please make sure all fields are filled out.");
-    }
-    /*
-    $.getJSON("./resources/json/loginData.json", function(result) {
-      if(!result.error) {
-        window.location.replace("/");
-      }
-      else {
-        alert("Login failed: Please check your credentials and try again");
-      }
-    });
-    */
-  });
+  // Old login
+  // $("#signIn").click(function() {
+  //   uname = $("#username").val();
+  //   pass = $("#password").val();
+  //   // Send Credentials to Server
+  //   if(uname && pass) {
+  //     $.ajax({
+  //       method    : "POST",
+  //       url       : "/login",
+  //       data      : {
+  //         "uname" : uname,
+  //         "pass"  : pass
+  //       },
+  //       success: function(message, status){
+  //         console.log(`Register request returns success with message ${message} and status ${status}.`);
+  //         alert("Login successful!");
+  //         window.location.href = "/user";
+  //       },
+  //       error: function(message, status){
+  //         console.log(`Oops! Register request returns failure with message ${message} and status ${status}.`);
+  //       }
+  //     });
+  //   }
+  //   else {
+  //     alert("Error: Please make sure all fields are filled out.");
+  //   }
+  //   /*
+  //   $.getJSON("./resources/json/loginData.json", function(result) {
+  //     if(!result.error) {
+  //       window.location.replace("/");
+  //     }
+  //     else {
+  //       alert("Login failed: Please check your credentials and try again");
+  //     }
+  //   });
+  //   */
+  // });
 }
