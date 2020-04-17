35064f96fef866721b7f3c4b85355063df68b816
sunzhaoxi99 <sunzhaoxi99@outlook.com>
2020-04-17 14:46:31 -0400

    friend page script

---
+++ resources/js/friendPage.js
@@ -0,0 +1,26 @@
+angular.module('friendApp', []).controller('friendController', ['$scope', '$http', function($scope, $http){
+    $scope.checkStatus = function(){
+        $http.get("/checkStatus").then(function(response){
+            $scope.status = response.data.status;
+            console.log($scope.status);
+            if($scope.status == null){
+                document.getElementById("account").style.display = "none";
+                document.getElementById("out").style.display = "none";
+            }
+            else{
+                document.getElementById("log").style.display = "none";
+                document.getElementById("sign").style.display = "none";
+            }
+
+        });
+    }
+                
+    $scope.signOut = function(){
+        alert("Signing user out...");
+        $http.get("/removeLogin").then(function(response){
+            console.log("removing logged in user");
+            console.log(response.data);
+        });
+        location.replace("/");
+    }
+}]);
