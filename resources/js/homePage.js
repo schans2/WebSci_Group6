3c06e369416e3a5e270bf299b75b11aaeb92b5f8
sunzhaoxi99 <sunzhaoxi99@outlook.com>
2020-04-17 15:07:37 -0400

    homePage Javascript

---
+++ resources/js/homePage.js
@@ -0,0 +1,43 @@
+angular.module('homeApp', []).controller('homeController', ['$scope', '$http', function($scope, $http){
+    $scope.checkStatus = function(){
+        $http.get("/checkStatus").then(function(response){
+            $scope.status = response.data.status;
+            console.log($scope.status);
+            if($scope.status == null){
+                document.getElementById("account").style.display = "none";
+                document.getElementById("out").style.display = "none";
+            }
+            else{
+                document.getElementById("log").style.display = "none";
+                document.getElementById("sign").style.display = "none";
+            }
+
+        });
+    }
+                
+    $scope.join = function(){
+        //alert("hello");
+        var code = document.getElementById("search").value;
+        if(code == ""){
+            alert("Please enter your code to continue...");
+        }
+        else{
+            $scope.code = {
+                joinCode: code,
+            }
+            $http.post("/joinGroup", $scope.code).then(function(response){
+                console.log("joining playlist...");
+            });
+            location.replace('./player');
+        }           
+    }
+                
+    $scope.signOut = function(){
+        alert("Signing user out...");
+        $http.get("/removeLogin").then(function(response){
+            console.log("removing logged in user");
+            console.log(response.data);
+        });
+        location.replace("/");
+    }
+}]);
